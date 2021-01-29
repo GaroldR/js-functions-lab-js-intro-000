9c09967caaa91a7784c4184d3003cba2448ceccf
Garold Rabinovich <garold.rabinovich@gmail.com>
2021-01-29 05:34:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// write your code below!
+function happyHolidays() {
+  return "Happy holidays!"}
+console.log(happyHolidays());
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`}
+console.log('happyHolidaysTo(name)');
+
+function happyCostumHolidayTo(holiday, name)
+  console.log(`Happy ${holiday}, ${name}!`);
